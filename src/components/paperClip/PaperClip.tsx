9a5d6e27d0cc4c195096d7108a1d8763e83f3a3b
noviceGuru <amin.arabi@gmail.com>
2023-10-31 09:33:19 +0100

    paperclip icon added

--- src/components/paperClip/PaperClip.tsx
+++ src/components/paperClip/PaperClip.tsx
@@ -1,6 +1,9 @@
+import PaperClipSvg from "../icons/PaperClipIcon"
+
 export default function PaperClip() {
     return (
-        <div className="bg-slate-400 opacity-60 cursor-pointer rounded-lg w-12 h-8">
+        <div className="bg-slate-400 opacity-60 cursor-pointer rounded-lg rotate-45">
+            <PaperClipSvg className="w-6"/>
         </div>
     )
 }
