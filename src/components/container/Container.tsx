4337391b29de24d03c223ee709d3848ab566febf
noviceGuru <amin.arabi@gmail.com>
2023-11-13 19:11:03 +0100

    Toggle button's border removed

--- src/components/container/Container.tsx
+++ src/components/container/Container.tsx
@@ -43,8 +43,7 @@ export default function JarContainer({
                 <label className="relative inline-flex items-center cursor-pointer">
                     <input type="checkbox" value="" onClick={()=>setShowTodo(!showTodo)} className="sr-only peer" />
                     <div
-                        className="w-40 h-6 bg-gray-200 peer-focus:outline-none peer-focus:ring-4 peer-focus:ring-blue-300 
-                dark:peer-focus:ring-lime-300 rounded-full
+                        className="w-40 h-6 bg-gray-200 rounded-full
                 dark:bg-gray-200 peer-checked:after:translate-x-[135px] peer-checked:after:border-white 
                 after:content-[''] after:absolute after:top-[2px] after:left-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full 
                 after:h-5 after:w-5 after:transition-all dark:border-gray-600 peer-checked:bg-lime-300 text-center"
