0c58049db5ac6f666f857b73de34c0fb4d24bd43
noviceGuru <amin.arabi@gmail.com>
2023-11-24 13:23:07 +0100

    google added as document.google, to prevent the error in production

--- src/App.tsx
+++ src/App.tsx
@@ -3,59 +3,54 @@ import Overlay from "./components/overlay/Overlay"
 import PaperClipTracker from "./components/paperClipTracker/PaperClipTracker"
 
 type JwtPayload = {
-    iss: string;
-    azp: string;
-    aud: string;
-    sub: string;
-    email: string;
-    email_verified: boolean;
-    nbf: number;
-    name: string;
-    picture: string;
-    given_name: string;
-    family_name: string;
-    locale: string;
-    iat: number;
-    exp: number;
-    jti: string;
-  }
+    iss: string
+    azp: string
+    aud: string
+    sub: string
+    email: string
+    email_verified: boolean
+    nbf: number
+    name: string
+    picture: string
+    given_name: string
+    family_name: string
+    locale: string
+    iat: number
+    exp: number
+    jti: string
+}
 
-  type Response = {
-    clientId: string;
-    client_id: string;
-    credential: string;
-    select_by: string;
-  }
+type Response = {
+    clientId: string
+    client_id: string
+    credential: string
+    select_by: string
+}
 
 function App() {
     const [userInfo, setUserInfo] = useState<JwtPayload | null>(null)
-    const [isLoggedIn, setIsLoggedIn] = useState<boolean>(false)
-    
-    const handleCallbackResponse = (response: Response) => {
-        setIsLoggedIn(true)
-        setUserInfo(parseJwt(response.credential))
-        console.log(parseJwt(response.credential))
-    }
-
-    const handleLogout = () => {
-        setUserInfo(null)
-        setIsLoggedIn(false)
-    }
-
-    function parseJwt(token: string) {
+    //@ts-ignore
+    const google = window.google
+    const parseJwt = (token: string) => {
         var base64Url = token.split(".")[1]
         var base64 = base64Url.replace(/-/g, "+").replace(/_/g, "/")
         var jsonPayload = decodeURIComponent(
             window
                 .atob(base64)
                 .split("")
-                .map(function (c) {
-                    return "%" + ("00" + c.charCodeAt(0).toString(16)).slice(-2)
-                })
+                .map(c => "%" + ("00" + c.charCodeAt(0).toString(16)).slice(-2))
                 .join("")
         )
 
         return JSON.parse(jsonPayload)
+    }
+
+    const handleCallbackResponse = (response: Response) => {
+        setUserInfo(parseJwt(response.credential))
+    }
+
+    const handleLogout = () => {
+        setUserInfo(null)
     }
 
     useEffect(() => {
@@ -71,13 +66,21 @@ function App() {
             theme: "outline",
             size: "large",
         })
-    }, [isLoggedIn])
+    }, [userInfo])
 
     return (
         <div className="h-full relative min-w-[290px] bg-sky-100">
-            {isLoggedIn ? <button onClick={handleLogout}>Logout</button> : <div id="signInDiv"></div>}
-            {userInfo && <h3>Welcome {userInfo.given_name.split(' ')[0]}</h3>}
-            {userInfo && <img src={userInfo.picture} className="rounded-full"/>}
+            {!!userInfo ? (
+                <button onClick={handleLogout}>Logout</button>
+            ) : (
+                <div className="w-8 bg-red-700">
+                    <div id="signInDiv" className="w-8"></div>
+                </div>
+            )}
+            {userInfo && <h3>Welcome {userInfo.given_name.split(" ")[0]}</h3>}
+            {userInfo && (
+                <img src={userInfo.picture} className="rounded-full" />
+            )}
             <Overlay />
             <PaperClipTracker />
         </div>
