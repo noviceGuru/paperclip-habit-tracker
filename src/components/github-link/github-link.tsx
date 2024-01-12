573d57c84185cbfa7062bbee52952175ed45ddb9
noviceGuru <amin.arabi@gmail.com>
2024-01-12 06:00:25 +0100

    github icon and signout button distance and size regulated for signed in responsive page

--- src/components/github-link/github-link.tsx
+++ src/components/github-link/github-link.tsx
@@ -3,11 +3,11 @@ import GitHubIcon from "../../assets/github-icon.svg"
 export default function GitHubLink() {
     return (
         <a
-            className="flex justify-center absolute right-2 top-1"
+            className="flex justify-center absolute right-6 top-6"
             href="https://github.com/noviceGuru/paperclip-habit-tracker"
             target="_blank"
         >
-            <img src={GitHubIcon} alt="show-list-button" className="m-6 w-8 sm:w-12" />
+            <img src={GitHubIcon} alt="show-list-button" className="w-8 h-8 md:w-10 md:h-10 cursor-pointer align-middle lg:w-12 lg:h-12 2xl:w-18 2xl:h-18" />
         </a>
     )
 }
